1a85041f44d613eb074a9c771cd014fb41c539bc
wail-10 <koundiwail02@gmail.com>
2024-03-22 02:31:14 +0000

    form patients controller

--- src/main/java/com/example/tpspringmvc/web/PatientController.java
+++ src/main/java/com/example/tpspringmvc/web/PatientController.java
@@ -8,6 +8,7 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
@@ -34,5 +35,17 @@ public class PatientController {
     public String delete(@RequestParam(name = "id") Long id,int page, String keyword){
         patientRepository.deleteById(id);
         return "redirect:/index?page="+page+"&keyword="+keyword;
+    }
+
+    @GetMapping("/formPatients")
+    public String formPatients(Model model){
+        model.addAttribute("patient", new Patient());
+        return "formPatients";
+    }
+
+    @PostMapping(path = "/save")
+    public String save(Model model, Patient patient){
+        patientRepository.save(patient);
+        return "formPatients";
     }
 }
